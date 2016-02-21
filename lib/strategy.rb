34747c9ecc19ddcc3c732d2399af9c5e3bb5ad3f
Jens Freudenau <jens@freude-now.de>
2016-02-21 19:49:54 +0100

    strategy

---
+++ lib/strategy.rb
@@ -0,0 +1,31 @@
+class Hero
+  attr_reader :damage, :health, :skills
+  attr_accessor :printer
+
+  def initialize(printer)
+    @damage = 10
+    @health = 5
+    @printer = printer
+    @skills = [:stealth, :driving, :intimidation]
+  end
+
+  def print_stats
+    if block_given?
+      yield(damage, health, skills)
+    else
+      printer.print(damage, health, skills)
+    end
+  end
+end
+
+class BattleStats
+  def print(damage, health, skills)
+    "Damage: #{damage}\nHealth: #{health}"
+  end
+end
+
+class SkillsStats
+  def print(damage, health, skills)
+    skills.inject('') { |result, skill| result + skill.to_s.capitalize + "\n" }
+  end
+end
---
+++ spec/strategy_spec.rb
@@ -0,0 +1,44 @@
+require 'rspec'
+require_relative '../lib/strategy'
+
+describe 'Strategy Pattern' do
+  describe Hero do
+    let(:hero) do
+      Hero.new (BattleStats.new)
+    end
+
+    it 'has damage' do
+      expect(hero.damage).to eq(10)
+    end
+    it 'has health' do
+      expect(hero.health).to eq(5)
+    end
+
+    describe 'Printing Character Stats' do
+      it 'prints battle stats by default' do
+        expect(hero.print_stats).to eq("Damage: 10\nHealth: 5")
+      end
+      it 'prints skills' do
+        hero.printer = SkillsStats.new
+        expect(hero.print_stats).to eq("Stealth\nDriving\nIntimidation\n")
+      end
+      it 'can use custom ad hoc printer' do
+        result = hero.print_stats do |damage, health, skills|
+          "Damage: #{damage}\nNumber of skills: #{skills.size}"
+        end
+        expect(result).to eq("Damage: 10\nNumber of skills: 3")
+      end
+    end
+    describe 'skills' do
+      it 'has stealth' do
+        expect(hero.skills).to include(:stealth)
+      end
+      it 'has driving' do
+        expect(hero.skills).to include(:driving)
+      end
+      it 'has intimidation' do
+        expect(hero.skills).to include(:intimidation)
+      end
+    end
+  end
+end
